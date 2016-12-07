09fbf34360a0658641a8746a5cae77336529a986
vladimirbahmetyev <vladimir240298perm@gmail.com>
2016-12-07 21:50:01 +0300

    ReRestructing old HomeWorks

--- 1st_course/1st_sem/hw4_2/Hw4_2_2/load.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-//Loading "sizeOfArray" elements from file to array
-void inputArrayFromFile(int array[], int sizeOfArray);
--- 1st_course/1st_sem/hw4_2/Hw4_2_2/sorts.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Sorts part of array from leftElement to rightElement
-void insertionSort(int array[], int leftElement, int rightElement);
-
-// Sorts sector of array since leftElement to rightElement (more quickly than insertion sort)
-void qSort(int array[], int leftElement, int rightElement);
