42fade9c3d73532f241b9cb384e26442329d7197
vladimirbahmetyev <vladimir240298perm@gmail.com>
2016-12-19 17:17:24 +0300

    Add new functions and finished module Graph.h

--- 1st_course/1st_sem/HomeWork_10/Hw10_1/Hw10_1/Source.cpp
+++ 1st_course/1st_sem/HomeWork_10/Hw10_1/Hw10_1/Source.cpp
@@ -1,4 +1,15 @@
 #include "Graph.h"
+#include <fstream>
 
+using namespace std;
 
-
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	ifstream inputFile("inputfile.txt");
+	int numberOfCountries = 0;
+	List **mapOfWorld = creatingCoutries(inputFile);
+	printMap(mapOfWorld);
+	deleteMap(mapOfWorld);
+	system("pause");
+}
