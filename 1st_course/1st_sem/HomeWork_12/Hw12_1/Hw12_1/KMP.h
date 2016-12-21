f9279f61c5c386e87c74ea2b22a54d27a28f8bf7
vladimirbahmetyev <vladimir240298perm@gmail.com>
2016-12-21 14:04:53 +0300

    Add file with module

---
+++ 1st_course/1st_sem/HomeWork_12/Hw12_1/Hw12_1/KMP.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int lengthOfString(string inputString);
+
+int KMP(ifstream &inputFile, string inputString);
+
+void test1();
+
+void test2();
+
+void test3();
